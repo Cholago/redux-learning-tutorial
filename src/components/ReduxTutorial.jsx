ef713ccb4517ff69cb8f18fb5fa2297b1fb39edd
Cholago <cholawilson@yahoo.com>
2020-08-08 21:38:26 +0300

    initial commit

---
+++ src/components/ReduxTutorial.jsx
@@ -0,0 +1,29 @@
+import React from 'react';
+import Store from './Store'
+import { bugAdded, bugRemoved } from './ActionCreactors'
+const ReduxTutorial = (props) => {
+
+    console.log("Store object!", Store);
+
+    //Notifies ui tha store data has changed
+    const unSubscribe = Store.subscribe(() => {
+        console.log("Store changed!", Store.getState());
+    });
+    //Add data to our redux store
+    bugAdded("What if am wrong and i will never be right");
+    bugAdded("Yello wilson Chola is cripmled hahahah");
+    //console.log(Store.getState());
+    //Removing data from our store
+    //unSubscribe(); //unsubscribe from being notifyied
+    bugRemoved(1)  //remove bug
+    //console.log(Store.getState());
+    unSubscribe();
+
+    return (
+        <div className="container">
+            <h1 className="text-center" >Redux tutorial</h1>
+        </div>
+
+    )
+}
+export default ReduxTutorial;
