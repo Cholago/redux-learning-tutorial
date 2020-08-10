2c9edac1c8a4d1e4e91d10470d70137c132b72ac
Cholago <cholawilson@yahoo.com>
2020-08-10 21:20:04 +0300

    fith commit

--- src/components/ReduxReact.jsx
+++ src/components/ReduxReact.jsx
@@ -14,9 +14,11 @@ const ReactRedux = ({ todos }) => {
         <div className="container">
             <div className="row">
                 <div className="col-md-6 ml-auto mr-auto">
-                    <ul className="list-group">
+                    <ul className="list-group" style={{
+                        'marginTop': '20px'
+                    }}>
                         <li className="list-group-item">
-                            <h2 className="text-center" >TODO APP</h2>
+                            <h2 className="text-center" >TODO APP REACT AND REDUX</h2>
                         </li>
                         <li className="list-group-item"><AddTodo /></li>
                         {todos.map(todo => <Todo key={todo.id} todoData={todo} />)}
